1baff824dbf93bee1372045954845086586e9462
ublzesc <68029970+ublzesc@users.noreply.github.com>
2021-02-20 18:22:20 -0500

    Update pystocktwits.py

--- pystocktwits/pystocktwits.py
+++ pystocktwits/pystocktwits.py
@@ -132,3 +132,43 @@ class Streamer():
 
         raw_json = r.json()
         return raw_json
+    
+    def get_all_msgs(self, symbol_id, since=0, max=0, limit=0, callback=None, filter=None):
+
+        '''Returns the most recent 30 messages in the platform.
+
+        Args:
+            since:	Returns results with an ID greater than (more recent than)
+                    the specified ID.
+            max:	Returns results with an ID less than (older than) or
+                    equal to the specified ID.
+            limit:	Default and max limit is 30. This limit must be a
+                    number under 30.
+            callback:	Define your own callback function name,
+                        add this parameter as the value.
+            filter:	Filter messages by links, charts, videos,
+                    or top. (Optional)
+
+        Return:
+            raw_json (dict) = The JSON output unparsed
+
+        '''
+
+        url = self.url + 'streams/all.json'
+
+        data = {
+                 'since': '{}'.format(since),
+                 'max': '{}'.format(max),
+                 'limit': '{}'.format(limit),
+                 # Fix when you figure out what this is
+                 # 'callback' : '{}'.format(None),
+                 'filter': '{}'.format(filter)
+                }
+
+        r = requests.get(url, headers=self.headers, params=data)
+        if r.status_code != 200:
+            raise Exception('Unable to Return Request {}'
+                            .format(r.status_code))
+
+        raw_json = r.json()
+        return raw_json
